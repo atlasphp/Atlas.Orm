7408c92b618004d92b6cd052a8759c2cd0c403b2
Paul M. Jones <pmjones88@gmail.com>
2015-10-02 15:17:43 -0500

    increase test coverage

---
+++ tests/Mapper/MapperLocatorTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace Atlas\Mapper;
+
+use Atlas\Exception;
+use Atlas\Fake\Employee\EmployeeMapper;
+use Atlas\Fake\Employee\EmployeeRecord;
+use Atlas\Fake\Employee\EmployeeRecordSet;
+use Atlas\Fake\Employee\EmployeeRow;
+use Atlas\Mapper\Related;
+
+class MapperLocatorTest extends \PHPUnit_Framework_TestCase
+{
+    protected $mapperLocator;
+
+    protected function setUp()
+    {
+        $this->mapperLocator = new MapperLocator();
+        $this->mapperLocator->set(EmployeeMapper::CLASS, function () {
+            return EmployeeMapper::CLASS;
+        });
+    }
+
+    public function testHas()
+    {
+        $this->assertFalse($this->mapperLocator->has('Atlas\Fake\Employee'));
+        $this->assertTrue($this->mapperLocator->has(EmployeeMapper::CLASS));
+        $this->assertTrue($this->mapperLocator->has(EmployeeRecord::CLASS));
+        $this->assertTrue($this->mapperLocator->has(EmployeeRecordSet::CLASS));
+    }
+
+    public function testGet()
+    {
+        $expect = EmployeeMapper::CLASS;
+
+        $this->assertSame($expect, $this->mapperLocator->get(EmployeeMapper::CLASS));
+        $this->assertSame($expect, $this->mapperLocator->get(EmployeeRecord::CLASS));
+        $this->assertSame($expect, $this->mapperLocator->get(EmployeeRecordSet::CLASS));
+
+        $object = new EmployeeRecord(new EmployeeRow([], 'id'), new Related([]));
+        $this->assertSame($expect, $this->mapperLocator->get($object));
+
+        $this->setExpectedException(Exception::CLASS, "Atlas\Fake\Employee not found in locator");
+        $this->mapperLocator->get('Atlas\Fake\Employee');
+    }
+}
