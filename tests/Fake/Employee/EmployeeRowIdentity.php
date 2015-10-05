5f46a92879badf591bfbcf2d7fe2cd34213164ee
Paul M. Jones <pmjones88@gmail.com>
2015-10-05 14:10:32 -0500

    add fake RowIdentity classes

---
+++ tests/Fake/Employee/EmployeeRowIdentity.php
@@ -0,0 +1,8 @@
+<?php
+namespace Atlas\Fake\Employee;
+
+use Atlas\Table\RowIdentity;
+
+class EmployeeRowIdentity extends RowIdentity
+{
+}
---
+++ tests/Fake/Tag/TagRowIdentity.php
@@ -0,0 +1,8 @@
+<?php
+namespace Atlas\Fake\Tag;
+
+use Atlas\Table\RowIdentity;
+
+class TagRowIdentity extends RowIdentity
+{
+}
