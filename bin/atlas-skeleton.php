38d18a1b67cd583cc1e0e4fdab05d73a270ae89f
Paul M. Jones <pmjones88@gmail.com>
2015-10-26 02:24:58 -0500

    find autoload in dfferent places

--- bin/atlas-skeleton.php
+++ bin/atlas-skeleton.php
@@ -1,20 +1,34 @@
+#!/usr/bin/env php
 <?php
-use Aura\Cli\CliFactory;
-use Atlas\Command\Skeleton;
+error_reporting(E_ALL);
 
-// @todo make allowances for normal installations
-require dirname(__DIR__) . "/vendor/autoload.php";
+$autoload = false;
 
-// create the command with its dependencies
-$cliFactory = new CliFactory();
-$command = new Skeleton(
+$files = array(
+    __DIR__ . '/../../../autoload.php',
+    __DIR__ . '/../../vendor/autoload.php',
+    __DIR__ . '/../vendor/autoload.php'
+);
+
+foreach ($files as $file) {
+    if (file_exists($file)) {
+        $autoload = $file;
+        break;
+    }
+}
+
+if (! $autoload) {
+    echo "Please install and update Composer before continuing." . PHP_EOL;
+    exit(1);
+}
+
+require $autoload;
+
+$cliFactory = new Aura\Cli\CliFactory();
+$command = new Atlas\Command\Skeleton(
     $cliFactory->newContext($GLOBALS),
     $cliFactory->newStdio(),
     getcwd()
 );
-
-// run the command and get its exit code
-$exit = $command();
-
-// done!
-exit($exit);
+$code = $command();
+exit($code);
