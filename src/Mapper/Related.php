ebbeb973f04613d318c5f9b7f3414846e4366adc
Paul M. Jones <pmjones88@gmail.com>
2015-11-18 12:16:19 -0600

    internal property rename

--- src/Mapper/Related.php
+++ src/Mapper/Related.php
@@ -5,35 +5,35 @@ use Atlas\Orm\Exception;
 
 class Related
 {
-    private $foreign = [];
+    private $fields = [];
 
-    public function __construct(array $foreign = [])
+    public function __construct(array $fields = [])
     {
-        $this->foreign = $foreign;
+        $this->fields = $fields;
     }
 
     public function __get($name)
     {
         $this->assertHas($name);
-        return $this->foreign[$name];
+        return $this->fields[$name];
     }
 
-    public function __set($name, $foreign)
+    public function __set($name, $fields)
     {
         $this->assertHas($name);
-        $this->foreign[$name] = $foreign;
+        $this->fields[$name] = $fields;
     }
 
     public function __isset($name)
     {
         $this->assertHas($name);
-        return isset($this->foreign[$name]);
+        return isset($this->fields[$name]);
     }
 
     public function __unset($name)
     {
         $this->assertHas($name);
-        $this->foreign[$name] = null;
+        $this->fields[$name] = null;
     }
 
     protected function assertHas($name)
@@ -46,13 +46,13 @@ class Related
 
     public function has($name)
     {
-        return array_key_exists($name, $this->foreign);
+        return array_key_exists($name, $this->fields);
     }
 
     public function getArrayCopy()
     {
         $array = [];
-        foreach ($this->foreign as $name => $foreign) {
+        foreach ($this->fields as $name => $foreign) {
             $array[$name] = $foreign;
             if ($foreign) {
                 $array[$name] = $foreign->getArrayCopy();
