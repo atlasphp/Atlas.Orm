e5166c151151e138a605579732bd469bda58fbe9
Paul M. Jones <pmjones88@gmail.com>
2016-10-10 13:25:24 -0500

    refactoring, consolidation, and docblocks

--- src/Mapper/MapperLocator.php
+++ src/Mapper/MapperLocator.php
@@ -8,67 +8,23 @@
  */
 namespace Atlas\Orm\Mapper;
 
+use Atlas\Orm\AbstractLocator;
 use Atlas\Orm\Exception;
 
 /**
  *
- * A ServiceLocator implementation for creating and retaining Mapper objects.
+ * A ServiceLocator for Mapper objects.
  *
  * @package atlas/orm
  *
  */
-class MapperLocator
+class MapperLocator extends AbstractLocator
 {
     /**
-     *
-     * A registry of callable factories to create Mapper instances.
-     *
-     * @var array
-     *
+     * @inheritdoc
      */
-    protected $factories = [];
-
-    /**
-     *
-     * A registry of Mapper instances created by the factories.
-     *
-     * @var array
-     *
-     */
-    protected $instances = [];
-
-    public function set($class, callable $factory)
+    protected function notFoundException($class)
     {
-        $this->factories[$class] = $factory;
-    }
-
-    public function has($class)
-    {
-        return isset($this->factories[$class]);
-    }
-
-    /**
-     *
-     * Gets a Mapper instance by class; if it has not been created yet, its
-     * callable factory will be invoked and the instance will be retained.
-     *
-     * @param string $class The class of the Mapper instance to retrieve.
-     *
-     * @return Mapper A Mapper instance.
-     *
-     * @throws Exception When an Mapper type is not found.
-     *
-     */
-    public function get($class)
-    {
-        if (! isset($this->factories[$class])) {
-            throw Exception::mapperNotFound($class);
-        }
-
-        if (! isset($this->instances[$class])) {
-            $this->instances[$class] = call_user_func($this->factories[$class]);
-        }
-
-        return $this->instances[$class];
+        return Exception::mapperNotFound($class);
     }
 }
--- src/Table/TableLocator.php
+++ src/Table/TableLocator.php
@@ -8,67 +8,23 @@
  */
 namespace Atlas\Orm\Table;
 
+use Atlas\Orm\AbstractLocator;
 use Atlas\Orm\Exception;
 
 /**
  *
- * A ServiceLocator implementation for creating and retaining Table objects.
+ * A ServiceLocator for Table objects.
  *
  * @package atlas/orm
  *
  */
-class TableLocator
+class TableLocator extends AbstractLocator
 {
     /**
-     *
-     * A registry of callable factories to create Table instances.
-     *
-     * @var array
-     *
+     * @inheritdoc
      */
-    protected $factories = [];
-
-    /**
-     *
-     * A registry of Table instances created by the factories.
-     *
-     * @var array
-     *
-     */
-    protected $instances = [];
-
-    public function set($class, callable $factory)
+    protected function notFoundException($class)
     {
-        $this->factories[$class] = $factory;
-    }
-
-    public function has($class)
-    {
-        return isset($this->factories[$class]);
-    }
-
-    /**
-     *
-     * Gets a Table instance by class; if it has not been created yet, its
-     * callable factory will be invoked and the instance will be retained.
-     *
-     * @param string $class The class of the Table instance to retrieve.
-     *
-     * @return Table A Table instance.
-     *
-     * @throws Exception When a Table instance is not found.
-     *
-     */
-    public function get($class)
-    {
-        if (! isset($this->factories[$class])) {
-            throw Exception::tableNotFound($class);
-        }
-
-        if (! isset($this->instances[$class])) {
-            $this->instances[$class] = call_user_func($this->factories[$class]);
-        }
-
-        return $this->instances[$class];
+        return Exception::tableNotFound($class);
     }
 }
