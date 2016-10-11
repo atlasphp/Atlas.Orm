1a727f44384e2738c1261049c02862531063cb2e
Paul M. Jones <pmjones88@gmail.com>
2016-10-11 16:48:38 -0500

    docblocks

--- src/Table/TableEvents.php
+++ src/Table/TableEvents.php
@@ -15,47 +15,73 @@ use PDOStatement;
 
 /**
  *
- * __________
+ * Default events to be invoked during Table operations.
  *
  * @package atlas/orm
  *
  */
 class TableEvents implements TableEventsInterface
 {
+    /**
+     * @inheritdoc
+     */
     public function beforeInsert(TableInterface $table, RowInterface $row)
     {
     }
 
+    /**
+     * @inheritdoc
+     */
     public function modifyInsert(TableInterface $table, RowInterface $row, Insert $insert)
     {
     }
 
+    /**
+     * @inheritdoc
+     */
     public function afterInsert(TableInterface $table, RowInterface $row, Insert $insert, PDOStatement $pdoStatement)
     {
     }
 
+    /**
+     * @inheritdoc
+     */
     public function beforeUpdate(TableInterface $table, RowInterface $row)
     {
     }
 
+    /**
+     * @inheritdoc
+     */
     public function modifyUpdate(TableInterface $table, RowInterface $row, Update $update)
     {
     }
 
+    /**
+     * @inheritdoc
+     */
     public function afterUpdate(TableInterface $table, RowInterface $row, Update $update, PDOStatement $pdoStatement)
     {
     }
 
+    /**
+     * @inheritdoc
+     */
     public function beforeDelete(TableInterface $table, RowInterface $row)
     {
     }
 
+    /**
+     * @inheritdoc
+     */
     public function modifyDelete(TableInterface $table, RowInterface $row, Delete $delete)
     {
     }
 
+    /**
+     * @inheritdoc
+     */
     public function afterDelete(TableInterface $table, RowInterface $row, Delete $delete, PDOStatement $pdoStatement)
     {
     }
-
 }
