bf08706db8b2f3b0cdbf6e1f24fcfdb7552d3767
Paul M. Jones <pmjones88@gmail.com>
2016-10-14 13:09:59 -0500

    add tests for new RecordSet functions

--- tests/Mapper/RecordSetTest.php
+++ tests/Mapper/RecordSetTest.php
@@ -27,8 +27,10 @@ class RecordSetTest extends \PHPUnit_Framework_TestCase
 
         $this->record = new Record('FakeMapper', $this->row, $this->related);
 
-        $newRecord = function ($fields = []) {
-            // nothing for now;
+        $newRecord = function ($cols = []) {
+            $row = new Row($cols);
+            $related = new Related(['zim' => null, 'irk' => null]);
+            return new Record('FakeMapper', $row, $related);
         };
 
         $this->recordSet = new RecordSet([$this->record], $newRecord);
@@ -66,5 +68,57 @@ class RecordSetTest extends \PHPUnit_Framework_TestCase
         unset($this->recordSet[0]);
         $this->assertFalse(isset($this->recordSet[0]));
         $this->assertTrue($this->recordSet->isEmpty());
+    }
+
+    public function testAppendNew()
+    {
+        $record = $this->recordSet->appendNew([
+            'id' => null,
+            'foo' => 'newfoo'
+        ]);
+
+        $this->assertCount(2, $this->recordSet);
+        $this->assertSame($record, $this->recordSet[1]);
+    }
+
+    public function testGetAndRemove()
+    {
+        $this->recordSet->appendNew(['id' => 2, 'foo' => 'bar1']);
+        $this->recordSet->appendNew(['id' => 3, 'foo' => 'bar2']);
+        $this->recordSet->appendNew(['id' => 4, 'foo' => 'bar3']);
+        $this->recordSet->appendNew(['id' => 5, 'foo' => 'bar1']);
+        $this->recordSet->appendNew(['id' => 6, 'foo' => 'bar2']);
+        $this->recordSet->appendNew(['id' => 7, 'foo' => 'bar3']);
+        $this->recordSet->appendNew(['id' => 8, 'foo' => 'bar1']);
+        $this->recordSet->appendNew(['id' => 9, 'foo' => 'bar2']);
+        $this->recordSet->appendNew(['id' => 10, 'foo' => 'bar3']);
+
+        $actual = $this->recordSet->getOneBy(['foo' => 'no-such-value']);
+        $this->assertFalse($actual);
+
+        $actual = $this->recordSet->getOneBy(['foo' => 'bar1']);
+        $this->assertSame(2, $actual->id);
+
+        $actual = $this->recordSet->getAllBy(['foo' => 'bar2']);
+        $this->assertCount(3, $actual);
+        $this->assertSame(3, $actual[2]->id);
+        $this->assertSame(6, $actual[5]->id);
+        $this->assertSame(9, $actual[8]->id);
+
+        $this->assertCount(10, $this->recordSet);
+
+        $actual = $this->recordSet->removeOneBy(['foo' => 'no-such-value']);
+        $this->assertFalse($actual);
+
+        $actual = $this->recordSet->removeOneBy(['foo' => 'bar1']);
+        $this->assertSame(2, $actual->id);
+        $this->assertCount(9, $this->recordSet);
+        $this->assertFalse(isset($this->recordSet[1]));
+
+        $actual = $this->recordSet->removeAllBy(['foo' => 'bar2']);
+        $this->assertCount(6, $this->recordSet);
+        $this->assertSame(3, $actual[2]->id);
+        $this->assertSame(6, $actual[5]->id);
+        $this->assertSame(9, $actual[8]->id);
     }
 }
