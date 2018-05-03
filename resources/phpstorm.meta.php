743f065239503c87315e58120996d8b4e0e6fdf8
Paul M. Jones <pmjones88@gmail.com>
2018-05-03 13:32:33 -0500

    add PHPStorm metadata resource

---
+++ resources/phpstorm.meta.php
@@ -0,0 +1,12 @@
+<?php
+namespace PHPSTORM_META;
+override(\Atlas\Orm\Atlas::mapper(0), map(['' => '@']));
+override(\Atlas\Orm\Atlas::newRecord(0), map(['' => '@Record']));
+override(\Atlas\Orm\Atlas::newRecordSet(0), map(['' => '@RecordSet']));
+override(\Atlas\Orm\Atlas::fetchRecord(0), map(['' => '@Record|null']));
+override(\Atlas\Orm\Atlas::fetchRecordBy(0), map(['' => '@Record|null']));
+override(\Atlas\Orm\Atlas::fetchRecords(0), map(['' => '@Record[]']));
+override(\Atlas\Orm\Atlas::fetchRecordsBy(0), map(['' => '@Record[]']));
+override(\Atlas\Orm\Atlas::fetchRecordSet(0), map(['' => '@RecordSet']));
+override(\Atlas\Orm\Atlas::fetchRecordSetBy(0), map(['' => '@RecordSet']));
+override(\Atlas\Orm\Atlas::select(0), map(['' => '@Select']));
